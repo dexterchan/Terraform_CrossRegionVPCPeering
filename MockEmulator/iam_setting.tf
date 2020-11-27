56cc67eebd63f09949cf64cf0a704a7ea23dbe77
dexterchan <dexterchan@gmail.com>
2020-11-27 20:40:00 +0800

    fix load balancer health check

--- MockEmulator/iam_setting.tf
+++ MockEmulator/iam_setting.tf
@@ -11,6 +11,12 @@ resource "aws_iam_role_policy" "ec2_ssm_managed_policy" {
   role   = aws_iam_role.ec2_mockemulator_instance_role.id
 }
 
+
+resource "aws_iam_role_policy_attachment" "ec2_mockemulator_putlogs_policy" {
+  role       = aws_iam_role.ec2_mockemulator_instance_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_iam_role_policy_attachment" "ec2_mockemulator_s3_readonly_policy" {
   role       = aws_iam_role.ec2_mockemulator_instance_role.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
@@ -23,48 +29,48 @@ resource "aws_iam_instance_profile" "instance_profile" {
 
 
 resource "aws_iam_role" "ecs_terraform_service_role" {
-    name = "ecs_terraform_service_role"
-    assume_role_policy = file("./modules/policies/ecs-trustEntity.json")
+  name               = "ecs_terraform_service_role"
+  assume_role_policy = file("./modules/policies/ecs-trustEntity.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_terraform_service_role_policy" {
-    role = aws_iam_role.ecs_terraform_service_role.id
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  role       = aws_iam_role.ecs_terraform_service_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 resource "aws_iam_role" "ecs_terraform_taskexecution_role" {
-    name = "ecs_terraform_taskexecution_role"
-    assume_role_policy = file("./modules/policies/ecs-tasks-trustEntity.json")
+  name               = "ecs_terraform_taskexecution_role"
+  assume_role_policy = file("./modules/policies/ecs-tasks-trustEntity.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_terraform_taskexecution_role_policy" {
-    role = aws_iam_role.ecs_terraform_taskexecution_role.id
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_terraform_taskexecution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role" "ecs_task_app_execution_role" {
-    name = "ecs_task_app_execution_role"
-    assume_role_policy = file("./modules/policies/ecs-tasks-trustEntity.json")
+  name               = "ecs_task_app_execution_role"
+  assume_role_policy = file("./modules/policies/ecs-tasks-trustEntity.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_s3_policy" {
-    role = aws_iam_role.ecs_task_app_execution_role.id
-    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.ecs_task_app_execution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_dynamodb_policy" {
-    role = aws_iam_role.ecs_task_app_execution_role.id
-    policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  role       = aws_iam_role.ecs_task_app_execution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_xraywrite_policy" {
-    role = aws_iam_role.ecs_task_app_execution_role.id
-    policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+  role       = aws_iam_role.ecs_task_app_execution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_ssm_policy"{
-    role = aws_iam_role.ecs_task_app_execution_role.id
-    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_ssm_policy" {
+  role       = aws_iam_role.ecs_task_app_execution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
 resource "aws_iam_policy" "ecs_task_app_execution_role_kms_policy" {
@@ -85,7 +91,7 @@ resource "aws_iam_policy" "ecs_task_app_execution_role_kms_policy" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_kms_policy"{
-    role = aws_iam_role.ecs_task_app_execution_role.id
-    policy_arn = aws_iam_policy.ecs_task_app_execution_role_kms_policy.arn
+resource "aws_iam_role_policy_attachment" "ecs_task_app_execution_role_kms_policy" {
+  role       = aws_iam_role.ecs_task_app_execution_role.id
+  policy_arn = aws_iam_policy.ecs_task_app_execution_role_kms_policy.arn
 }
