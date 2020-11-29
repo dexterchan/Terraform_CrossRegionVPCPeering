db44ac060b7cdc348bf0d80f6b45317cfc8c7ac5
dexterchan <dexterchan@gmail.com>
2020-11-29 23:29:43 +0800

    add lambda

--- CanaryProbeFunction_TF_Deploy/lambda.tf
+++ CanaryProbeFunction_TF_Deploy/lambda.tf
@@ -1,29 +1,27 @@
+provider "aws" {
+  region = var.region
+}
 
 resource "aws_lambda_function" "remote_canary" {
   function_name = "${var.lambda_id}_CanaryProbeFunction"
-  s3_bucket = var.s3_bucket
-  s3_key    = var.s3_key
-  handler = var.lambda_handler
-  runtime = var.lambda_runtime
-  timeout = var.timeout
-  memory_size=var.lambda_memory_size
-  role = var.lambda_execrole
+  s3_bucket     = var.s3_bucket
+  s3_key        = var.s3_key
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+  timeout       = var.timeout
+  memory_size   = var.lambda_memory_size
+  role          = var.lambda_execrole
 
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.securitygroup_ids
+  }
   environment {
     variables = {
       ZFP = var.zfp
-      BBG_CLIENTCERT_KEYID="/mktsvc/dev/BBG_CLIENTCERT_PWD"
     }
   }
 }
-/*
-resource "aws_lambda_alias" "remote_canary_alias" {
-  name             = "${var.lambda_id}_CanaryProbeFunction_alias"
-  description      = "a sample description"
-  function_name    = aws_lambda_function.remote_canary.function_name
-  function_version = "$LATEST"
-}*/
-
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
@@ -34,8 +32,8 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
 }
 
 resource "aws_cloudwatch_event_rule" "schedule_run" {
-  name                = "EventBridgeEventRule"
-  description         = "run lambda regularly"
+  name        = "EventBridgeEventRule"
+  description = "run lambda regularly"
   //schedule_expression = "cron(0/1 0 * ? * * *)"
   //schedule_expression = "cron(0 */5 * ? * *)"
   schedule_expression = var.run_schedule
--- CanaryProbeFunction_TF_Deploy/variables.tf
+++ CanaryProbeFunction_TF_Deploy/variables.tf
@@ -1,44 +1,66 @@
-variable "lambda_handler"{
-  type = string
+variable "lambda_handler" {
+  type    = string
+  default = "canaryprobe.App::handleRequest"
 }
-variable "lambda_runtime"{
-  type = string
+variable "lambda_runtime" {
+  type    = string
+  default = "java11"
 }
-variable "lambda_memory_size"{
-  type = number
+variable "lambda_memory_size" {
+  type    = number
+  default = 512
 }
-variable "lambda_execrole"{
+variable "lambda_execrole" {
   description = "arn of lambda execution role"
+  type        = string
+}
+
+variable "lambda_id" {
   type = string
 }
 
-variable "lambda_id"{
+variable "s3_bucket" {
   type = string
 }
-
-variable "s3_bucket"{
+variable "s3_key" {
   type = string
 }
-variable "s3_key"{
-  type = string
-}
-variable "timeout"{
-  type = number
+variable "timeout" {
+  type    = number
   default = 360
 }
-variable "zfp"{
+variable "zfp" {
   type = bool
 }
-variable "region"{
+variable "region" {
   type = string
 }
 
-variable "zfp_cert_bucket"{
+variable "zfp_cert_bucket" {
   description = "zfp certificate s3 bucket"
-  type=string
+  type        = string
+  default     = ""
 }
 
-variable "run_schedule"{
+variable "run_schedule" {
   description = "run schedule of lambda by Eventbridge"
-  default = "rate(5 minutes)"
+  default     = "rate(5 minutes)"
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "securitygroup_ids" {
+  type = list(string)
+}
+
+variable "host" {
+  type = string
+}
+variable "port" {
+  type = number
+}
+variable "auth" {
+  type = string
 }
